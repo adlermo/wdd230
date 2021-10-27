3d6cc6587324b53bd6650d8f6103229542e2b38b
adlermo <adlerorteney@gmail.com>
2021-10-26 22:50:26 -0300

    Updating treshold and rootMargin

--- pages/lesson7/js/index.js
+++ pages/lesson7/js/index.js
@@ -18,8 +18,8 @@ const observer = new IntersectionObserver(
     });
   },
   {
-    threshold: 1,
-    rootMargin: "0px 0px 50px 0px",
+    threshold: 0,
+    rootMargin: "0px 0px 100px 0px",
   }
 );
 
