dd25b109f1b6aec7f8bd44495154140f3c1b1776
adlermo <adlerorteney@gmail.com>
2021-10-30 21:34:25 -0300

    created gallery 7 page

---
+++ pages/lesson7/js/min-nav.js
@@ -0,0 +1,13 @@
+const openMenu = document.getElementById("navMobile");
+const menuBar = document.getElementById("toggleMenu");
+
+openMenu.addEventListener("change", () => {
+  // Open menu checked, open menu
+  menuBar.toggleAttribute("hidden");
+});
+
+menuBar.addEventListener("change", () => {
+  // Menu option chosen, checking it
+  menuBar.toggleAttribute("hidden");
+  openMenu.firstChild.nextElementSibling.innerText = "Menu";
+});
