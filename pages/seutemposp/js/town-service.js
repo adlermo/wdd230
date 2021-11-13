81b6360a2ec1ef1c3bf0bbe996fa7bda18d85616
adlermo <adlerorteney@gmail.com>
2021-11-13 20:06:06 -0300

    Home page adding all cities at the json

---
+++ pages/seutemposp/js/town-service.js
@@ -0,0 +1,71 @@
+// Global towns parent section element
+const towns = document.getElementById("towns");
+
+const getAll = () => {
+  // URL to request town data
+  const url = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+  fetch(url)
+    .then(
+      (res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      },
+      (err) => {
+        alert("It looks like the data isn't available. Please check the log!");
+        console.log(err);
+      }
+    )
+    .then((json) => populateData(json));
+};
+
+/**
+ * Receives the json resolved method and iterates over each town creating the card
+ *
+ * @param {*} list
+ */
+const populateData = (list) => {
+  // Deconstructing object attribute towns
+  const { towns } = list;
+
+  towns.forEach((town) => createTown(town));
+};
+
+const createTown = (town) => {
+  const h1 = document.createElement("h1");
+  h1.textContent = town.name;
+  const h6 = document.createElement("h6");
+  h6.textContent = town.motto;
+
+  const p1 = document.createElement("p");
+  p1.textContent = `Year Founded: ${town.yearFounded}`;
+  const p2 = document.createElement("p");
+  p2.textContent = `Population: ${town.currentPopulation}`;
+  const p3 = document.createElement("p");
+  p3.textContent = `Annual Rain Fall: ${town.averageRainfall}`;
+
+  const img = document.createElement("img");
+  img.setAttribute("src", `../seutemposp/images/${town.photo}`);
+  img.setAttribute("alt", `${town.name} Image`);
+  img.setAttribute("width", `100%`);
+  img.setAttribute("heigth", `auto`);
+
+  // Containerin
+  const content = document.createElement("div");
+  content.appendChild(h1);
+  content.appendChild(h6);
+  content.appendChild(p1);
+  content.appendChild(p2);
+  content.appendChild(p3);
+
+  const parent = document.createElement("section");
+
+  parent.appendChild(content);
+  parent.appendChild(img);
+
+  towns.appendChild(parent);
+};
+
+// Main method call
+getAll();
